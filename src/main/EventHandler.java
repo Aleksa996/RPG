236a5718094c1e5428b93d2fa1ed348da502c249
Aleksa Kundačina <aleksa.kundacina@energosoft.rs>
2023-06-13 11:46:34 +0200

    pre render order

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -6,6 +6,9 @@ public class EventHandler {
 
     GamePanel gp;
     EventRect eventRect[][];
+
+    int previusEventX, previusEventY;
+    boolean canTouchEvent = true;
     public EventHandler(GamePanel gp){
         this.gp = gp;
         eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
@@ -20,15 +23,31 @@ public class EventHandler {
             eventRect[col][row].height = 5;
             eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
             eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if(col == gp.maxWorldCol){
+                col = 0;
+                row++;
+            }
         }
     }
 
     public void checkEvent(){
-        if(hit(27, 16, "right")){
-            damagePit(gp.dialougeState);
+        //CHECK IF PLAYER CHARACTER IS MORE THAN 1 TILE AWAY FROM THE LAST EVENT
+        int xDistance = Math.abs(gp.player.worldX - previusEventX);
+        int yDistance = Math.abs(gp.player.worldY - previusEventY);
+        int distance = Math.max(xDistance,yDistance);
+        if(distance > gp.tileSize){
+            canTouchEvent = true;
         }
-        if(hit(23,12,"up")){
-            healingPool(gp.dialougeState);
+
+        if(canTouchEvent){
+            if(hit(27, 16, "right")){
+                damagePit(27,16,gp.dialougeState);
+            }
+            if(hit(23,12,"up")){
+                healingPool(23,12,gp.dialougeState);
+            }
         }
     }
 
@@ -41,9 +60,12 @@ public class EventHandler {
         eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
         eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if(gp.player.solidArea.intersects(eventRect[col][row])){
+        if(gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false){
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
+
+                previusEventX = gp.player.worldX;
+                previusEventY = gp.player.worldY;
             }
         }
 
@@ -55,13 +77,15 @@ public class EventHandler {
         return hit;
     }
 
-    public void damagePit(int gameState){
+    public void damagePit(int col, int row, int gameState){
         gp.gameState = gameState;
         gp.ui.currentDialouge = "You fall into a pit";
         gp.player.life -= 1;
+        //eventRect[col][row].eventDone = true;
+        canTouchEvent = false;
     }
 
-    public void healingPool(int gameState){
+    public void healingPool(int col, int row, int gameState){
         if(gp.keyH.enterPressed){
             gp.gameState = gameState;
             gp.ui.currentDialouge = "You drink water";
